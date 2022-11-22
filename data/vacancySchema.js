ad360f7a92a3f20270029b74cd359c8fcdf2a781
Maksim Sosnovskiy <maxim.sosnovsky@gmail.com>
2022-11-22 10:39:58 +0300

    Updates

--- data/vacancySchema.js
+++
@@ -1,7 +0,0 @@
-const vacancySchema =
-{
-  experience: 'Требуемый опыт работы: ',
-  qualification: 'Необходимая квалификация:',
-  responsibilities: 'Обязанности:',
-  conditions: 'Условия труда:'
-}
